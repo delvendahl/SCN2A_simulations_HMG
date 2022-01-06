e2f8662b24da1240ea660856f5334c982ab311d9
IGOR DELVENDAHL <igordelvendahl@gmail.com>
2022-01-06 16:05:51 +0100

    remove unneccessary imports

--- plot.py
+++ plot.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 
 
 def plot(x, y, xlabel, ylabel, title, filename):
@@ -20,7 +19,6 @@ AIS_Na_current = np.loadtxt(f'{target_path}/ais_Na_current.txt', delimiter='\t')
 results = np.loadtxt(f'{target_path}/spiking_results.txt', delimiter='\t')
 
 plot(soma_AP_traces[:, 0], soma_AP_traces[:, 13], 'Time (ms)', 'Voltage (mV)', 'Soma AP Traces', f'{target_path}/soma_AP_traces.png')
-
 plot(AIS_Na_current[:, 0], AIS_Na_current[:, 13], 'Time (ms)', 'Current (nA)', 'AIS Na Current', f'{target_path}/ais_Na_current.png')
 
 plot(results[:, 0], results[:, 1], 'Current (nA)', 'Spike Count', 'Spiking Results', f'{target_path}/spiking_results.png')
